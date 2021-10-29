18a4c3384508d01171f284a58c84fdc20d42fb6a
Vitalii Valkov <vitramir@icloud.com>
2021-10-29 16:38:10 +0300

    add tests

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,5 +1,13 @@
-const { add } = require("../utils");
+const { add, subtract, divide } = require("../utils");
 
 it("add should return sum", () => {
   expect(add(1, 2)).toBe(3);
+});
+
+it("subtract", () => {
+  expect(subtract(10, 3)).toBe(7);
+});
+
+it("divide", () => {
+  expect(divide(10, 5)).toBe(2);
 });
